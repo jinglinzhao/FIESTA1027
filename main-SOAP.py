1d074c8f508c296053b3cbdefdbfd0c1e3deee55
AƧTЯOƆAT <jinglinzhao@gmail.com>
2021-10-05 18:09:09 -0400

    Update plots

--- main-SOAP.py
+++ main-SOAP.py
@@ -74,7 +74,7 @@ for n in range(N_file):
 # Feed CCFs into FIESTA
 #==============================================================================
 # shift_spectrum, err_shift_spectrum, power_spectrum, err_power_spectrum, RV_gauss = FIESTA(V_grid, CCF, eCCF, template=CCF[:,0])
-shift_spectrum, power_spectrum, RV_gauss = FIESTA(V_grid, CCF, eCCF, template=CCF[:,0])
+shift_spectrum, _, power_spectrum, _, RV_gauss = FIESTA(V_grid, CCF, eCCF, template=CCF[:,0], k_max=50)
 
 # Convertion from km/s to m/s
 shift_spectrum = shift_spectrum * 1000
@@ -99,10 +99,10 @@ colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
               '#bcbd22', '#17becf']
 
 rotation_phase = np.arange(N_file)/100
-alpha =1
+alpha = 0.7
 fig, axes = plt.subplots(figsize=(10, 6))
 for i in range(5):
-	plt.plot(rotation_phase, shift_spectrum[:,i] - shift_spectrum[0,i], color=colors[i], marker='o', alpha=alpha, label=r'$k={}$'.format(i+1))
+	plt.plot(rotation_phase, shift_spectrum[i,:] - shift_spectrum[i,0], color=colors[i], lw=4-i*0.7, marker='o', alpha=1-i*0.2, label=r'$k={}$'.format(i+1))
 plt.xlim([0.2,0.8])
 plt.xlabel('Rotation phase')
 plt.ylabel(r'$RV_{FT, k}$ [m/s]')
@@ -110,15 +110,15 @@ plt.legend()
 plt.savefig('FIESTA_II_insight_RV_k.png')
 plt.show()
 
-rv_weighted = np.zeros(shift_spectrum.shape[0])
-for i in range(shift_spectrum.shape[0]):
-	rv_weighted[i] = sum(shift_spectrum[i] * power_spectrum[i,:]) / np.sum(power_spectrum[i,:])
+rv_weighted = np.zeros(shift_spectrum.shape[1])
+for i in range(shift_spectrum.shape[1]):
+	rv_weighted[i] = sum(shift_spectrum[:,i] * power_spectrum[:,i]) / np.sum(power_spectrum[:,i])
 
 fig, axes = plt.subplots(figsize=(10, 6))
-plt.plot(rotation_phase, rv_weighted-rv_weighted[0], marker='s', alpha=alpha, label='Weighted mean')
+plt.plot(rotation_phase, rv_weighted-rv_weighted[0], marker='o', lw=3, color='black', alpha=1, label='Weighted mean')
 # plt.scatter(rotation_phase, rv_weighted-rv_weighted[0], marker='s', s=100, label='Weighted mean', alpha=0.5)
 # plt.plot(rotation_phase, RV_gauss-RV_gauss[0], marker='o', alpha=alpha, label=r'$RV_{gaussian}$')
-plt.scatter(rotation_phase, RV_gauss-RV_gauss[0], marker='o', s=150, color='red', label=r'$RV_{gaussian}$', alpha=0.5)
+plt.scatter(rotation_phase, RV_gauss-RV_gauss[0], marker='X', s=150, color='black', lw=1, label=r'$RV_{gaussian}$', alpha=0.5)
 plt.xlabel('Rotation phase')
 plt.ylabel('RV [m/s]')
 plt.xlim([0.2,0.8])
@@ -129,7 +129,7 @@ plt.show()
 #--------------------#
 # A challenging test #
 #--------------------#
-shift_spectrum, power_spectrum, RV_gauss = FIESTA(V_grid, CCF, eCCF, template=CCF[:,0])
+shift_spectrum, _, power_spectrum, _, RV_gauss = FIESTA(V_grid, CCF, eCCF, template=CCF[:,0])
 # Convertion from km/s to m/s
 shift_spectrum = shift_spectrum * 1000
 # err_shift_spectrum = err_shift_spectrum * 1000
@@ -141,7 +141,7 @@ def gaussian2(x, amp, mu, sig, c):
     return amp * np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.))) + c
 
 f = interp1d(V_grid0, CCF0[:,0], kind='cubic')
-ccf_shift = f(V_grid - RV_gauss[52]/1000)  # use interpolation function returned by `interp1d`
+ccf_shift = f(V_grid - RV_gauss[51]/1000)  # use interpolation function returned by `interp1d`
 ADD_NOISE = False
 if ADD_NOISE == True:
 	ccf_shift = np.random.normal(ccf_shift, (1 - ccf_shift) ** 0.5 / 10000)
@@ -155,7 +155,7 @@ print(RV_shift*1000)
 CCF_plot = np.zeros((CCF.shape[0],3))
 CCF_plot[:,0] = CCF[:,0]
 CCF_plot[:,1] = ccf_shift
-CCF_plot[:,2] = CCF[:,52]
+CCF_plot[:,2] = CCF[:,51]
 eCCF_plot = np.zeros(CCF_plot.shape)
 SN 			= 100000000
 for n in range(3):
@@ -163,98 +163,111 @@ for n in range(3):
 # CCF_plot 	= np.hstack((CCF_plot, CCF))
 # eCCF_plot 	= np.hstack((eCCF_plot, eCCF))
 
-shift_spectrum, err_shift_spectrum, power_spectrum, err_power_spectrum, RV_gauss = FIESTA(V_grid, CCF_plot, eCCF_plot, template=CCF[:,0], out = 5)
+shift_spectrum, err_shift_spectrum, power_spectrum, err_power_spectrum, RV_gauss = FIESTA(V_grid, CCF_plot, eCCF_plot, template=CCF_plot[:,0], k_max=6)
 
-SN 			= 100000
-eCCF_plot_noise = np.zeros(CCF_plot.shape)
-for n in range(3):
-	eCCF_plot_noise[:,n]= np.random.normal(0, (1-CCF_plot[:,n])**0.5/SN)
-shift_spectrum2, err_shift_spectrum2, power_spectrum2, err_power_spectrum2, RV_gauss2 = FIESTA(V_grid, CCF_plot, eCCF_plot_noise, template=CCF[:,0], out = 6)
+# SN 			= 100000
+# eCCF_plot_noise = np.zeros(CCF_plot.shape)
+# for n in range(3):
+# 	eCCF_plot_noise[:,n]= np.random.normal(0, (1-CCF_plot[:,n])**0.5/SN)
+# shift_spectrum2, err_shift_spectrum2, power_spectrum2, err_power_spectrum2, RV_gauss2 = FIESTA(V_grid, CCF_plot, eCCF_plot_noise, template=CCF[:,0], k_max = 6)
 
 
 spacing = np.diff(V_grid)[0]
 
-power0, phase0, freq = FT(CCF[:,0], spacing)
-power1, phase1, freq = FT(ccf_shift, spacing)
-power2, phase2, freq = FT(CCF[:,52], spacing)
-idx = np.arange(shift_spectrum2.shape[1])
+power0, phase0, freq = FT(CCF[:,0], spacing)	# template CCF
+power1, phase1, freq = FT(CCF_plot[:,1], spacing)	# shifted CCF 
+power2, phase2, freq = FT(CCF_plot[:,2], spacing)	# deformed CCF
+idx = np.arange(k_max)
 
+diff_phase = wrap(phase2-phase0)
+diff_phase_shift = wrap(phase1-phase0)
 
-ccf0 = CCF[:,0]
 x = V_grid
 
-plt.rcParams.update({'font.size': 18})
-fig, axes = plt.subplots(figsize=(18, 11))
-plt.subplots_adjust(hspace=0.3, wspace=0.5)  # the amount of width and height reserved for blank space between subplots
-# linewidth=3
+plt.rcParams.update({'font.size': 16})
+fig = plt.figure(figsize=(18, 11))
+# fig, axes = plt.subplots(figsize=(18, 11))
+plt.subplots_adjust(hspace=0.4, wspace=0.5)  # the amount of width and height reserved for blank space between subplots
+lw=3
+
+from matplotlib.ticker import FormatStrFormatter
+def mode_number(x):
+	return x / np.diff(freq)[0]
+inverse = mode_number
 
 plt.subplot(231)
-plt.plot(x, CCF_plot[:,2], 'r', alpha=alpha)
-plt.plot(x, CCF_plot[:,1], 'b--', alpha=alpha)
+plt.plot(x, CCF_plot[:,2], 'r', alpha=alpha, lw=lw)
+plt.plot(x, CCF_plot[:,1], 'b--', alpha=alpha, lw=lw)
 # plt.plot(x, CCF_plot[:,1]+eCCF_plot_noise[:,1], 'b.', alpha=0.3)
 # plt.plot(x, CCF_plot[:,2]+eCCF_plot_noise[:,2], 'r.', alpha=0.3)
-plt.title('Signal (CCF)')
+# plt.title('Signal (CCF)')
 plt.xlabel(r'$v$ [km/s]')
-plt.ylabel('Normalized flux')
+plt.ylabel('CCF (inverted)')
 plt.grid(True)
 
 # Singal deformation #
 plt.subplot(234)
-plt.plot(x, CCF_plot[:,2] - CCF_plot[:,0], 'r', alpha=alpha)
-plt.plot(x, CCF_plot[:,1] - CCF_plot[:,0], 'b--', alpha=alpha)
+plt.plot(x, CCF_plot[:,2] - CCF_plot[:,0], 'r', alpha=alpha, lw=lw)
+plt.plot(x, CCF_plot[:,1] - CCF_plot[:,0], 'b--', alpha=alpha, lw=lw)
 # plt.plot(x, CCF_plot[:,1] - CCF_plot[:,0] + eCCF_plot_noise[:,1] - eCCF_plot_noise[:,0], 'b-', alpha=0.3)
 # plt.plot(x, CCF_plot[:,2] - CCF_plot[:,0] + eCCF_plot_noise[:,2] - eCCF_plot_noise[:,0], 'r-', alpha=0.3)
-plt.title('CCF deformation')
 plt.xlabel(r'$v$ [km/s]')
-plt.ylabel('Flux difference')
+plt.ylabel(r'$\Delta$CCF')
 plt.grid(True)
 
 
 # power spectrum #
-plt.subplot(232)
-plt.plot(freq[idx], power2[idx], 'rs-', alpha=alpha)
-plt.plot(freq[idx], power1[idx], 'bo--', alpha=alpha)
+ax = fig.add_subplot(232)
+ax.plot(freq[idx], power2[idx], 'rs-', alpha=alpha, lw=lw)
+ax.plot(freq[idx], power1[idx], 'bo--', alpha=alpha, lw=lw)
 # plt.errorbar(freq[idx], power2[idx], err_power_spectrum2[2,idx], c='r', marker='s', ms=5, alpha=0.8)
 # plt.errorbar(freq[idx], power1[idx], err_power_spectrum2[1,idx], c='b', marker='o', ms=5, alpha=0.8)
 # plt.errorbar(freq[idx], diff_phase_shift[idx], err_shift_spectrum2[1,idx] * (2 * np.pi * freq[idx]),  marker='o', ms=5, alpha=0.8)
 # plt.errorbar(freq[idx], diff_phase[idx], err_shift_spectrum2[1,idx] * (2 * np.pi * freq[idx]), c='r', marker='s', ms=5, alpha=0.8)
-
-plt.title('Amplitude')
-plt.xlabel(r'$\xi$ [s/km]')
-plt.ylabel('Amplitude')
-plt.grid(True)
-
+ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+ax.set_xticks(freq[idx], minor=False)
+ax.set_xlabel(r'$\xi$ [s/km]')
+ax.set_ylabel(r'$A_k$')
+ax.grid(b=True, which='both')
+secax = ax.secondary_xaxis('top', functions=(mode_number, inverse))	
+secax.set_xticks(idx, minor=False)
+secax.set_xlabel(r'$k$')
 
 
 # power spectrum difference#
-plt.subplot(233)
-plt.plot(freq[idx], power2[idx]-power0[idx], 'rs-', alpha=alpha)
-plt.plot(freq[idx], power1[idx]-power0[idx], 'bo--', alpha=alpha)
-plt.title('Amplitude difference')
-plt.xlabel(r'$\xi$ [s/km]')
-plt.ylabel(r'$\Delta$A')
-plt.grid(True)
+
+ax = fig.add_subplot(233)
+ax.plot(freq[idx], power2[idx]-power0[idx], 'rs-', alpha=alpha, lw=lw)
+ax.plot(freq[idx], power1[idx]-power0[idx], 'bo--', alpha=alpha, lw=lw)
+ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+ax.set_xticks(freq[idx], minor=False)
+ax.set_xlabel(r'$\xi$ [s/km]')
+ax.set_ylabel(r'$\Delta A_k$')
+ax.grid(b=True, which='both')
+secax = ax.secondary_xaxis('top', functions=(mode_number, inverse))	
+secax.set_xticks(idx, minor=False)
+secax.set_xlabel(r'$k$')
 
 
-# differential phase spectrum
-# freq = (np.arange(len(x))+1) / ((max(x) - min(x)) / (len(x)-1) * len(x))
-# idx = 0:5
-idx = np.arange(shift_spectrum2.shape[1])
-plt.subplot(235)
-diff_phase = np.unwrap(phase2) - np.unwrap(phase0)  # Necessary! Don't use # diff_phase = phase - phase_tpl
-diff_phase_shift = np.unwrap(phase1) - np.unwrap(phase0)
-plt.plot(freq[idx], diff_phase[idx], 'rs-', alpha=alpha)
-plt.plot(freq[idx], diff_phase_shift[idx], 'bo--', alpha=alpha)
-# plt.errorbar(freq[idx], diff_phase_shift[idx], err_shift_spectrum2[1,idx] * (2 * np.pi * freq[idx]), c='b', marker='o', ms=5, alpha=0.8)
-# plt.errorbar(freq[idx], diff_phase[idx], err_shift_spectrum2[1,idx] * (2 * np.pi * freq[idx]), c='r', marker='s', ms=5, alpha=0.8)
-# plt.errorbar(freq[idx], diff_phase[idx]*1000, err_shift_spectrum2[2,idx]*1000, c='r', marker='s', ms=10, alpha=0.8)
-plt.title('Phase shift')
-plt.xlabel(r'$\xi$ [s/km]')
-plt.ylabel(r'$\Delta \phi$ [radian]')
-plt.grid(True)
+
+
+ax = fig.add_subplot(235)
+
+ax.plot(freq[idx], diff_phase[idx], 'rs-', alpha=alpha, lw=lw)
+ax.plot(freq[idx], diff_phase_shift[idx], 'bo--', alpha=alpha, lw=lw)
+ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+ax.set_xticks(freq[idx], minor=False)
+ax.set_xlabel(r'$\xi$ [s/km]')
+ax.set_ylabel(r'$\Delta \phi_k$ [radian]')
+ax.grid(b=True, which='both')
+secax = ax.secondary_xaxis('top', functions=(mode_number, inverse))	
+secax.set_xticks(idx, minor=False)
+secax.set_xlabel(r'$k$')
+
+
 
 # shift spectrum #
-plt.subplot(236)
+ax = fig.add_subplot(236)
 
 rv = np.zeros(len(diff_phase))
 rv[1:] = - diff_phase[1:] / (2 * np.pi * freq[1:])
@@ -264,38 +277,24 @@ rv_shift = np.zeros(len(diff_phase_shift))
 rv_shift[1:] = - diff_phase_shift[1:] / (2 * np.pi * freq[1:])
 # rv_shift[0] = rv_shift[1]
 
-idx = np.arange(shift_spectrum2.shape[1]-1)
-plt.plot(freq[idx+1], rv[idx+1] * 1000, 'rs-', alpha=alpha)
-plt.plot(freq[idx+1], rv_shift[idx+1] * 1000, 'bo--', alpha=alpha)
+# idx = np.arange(shift_spectrum2.shape[1]-1)
+ax.plot(freq[1:6], rv[1:6] * 1000, 'rs-', alpha=alpha, lw=lw)
+ax.plot(freq[1:6], rv_shift[1:6] * 1000, 'bo--', alpha=alpha, lw=lw)
 # plt.errorbar(freq[idx+1], rv[idx+1]*1000, err_shift_spectrum2[2,idx+1]*1000, c='r', marker='s', ms=5, alpha=0.8)
 # plt.errorbar(freq[idx+1], rv_shift[idx+1]*1000, err_shift_spectrum2[1,idx+1]*1000, c='b', marker='o', ms=5, alpha=0.8)
-plt.title('RV Shift')
-plt.xlabel(r'$\xi$ [s/km]')
-plt.ylabel(r'$\Delta$RV [m/s]')
-plt.grid(True)
+ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+ax.set_xticks(freq[idx], minor=False)
+ax.set_xlabel(r'$\xi$ [s/km]')
+ax.set_ylabel(r'$\Delta RV_k$ [m/s]')
+ax.grid(b=True, which='both')
+secax = ax.secondary_xaxis('top', functions=(mode_number, inverse))	
+secax.set_xticks(idx, minor=False)
+secax.set_xlabel(r'$k$')
 if ADD_NOISE == False:
 	plt.savefig('comparison_noise-free.png')
 else:
 	plt.savefig('comparison_noise-added.png')
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 #=================#
@@ -324,7 +323,7 @@ plt.show()
 
 # fig, axes = plt.subplots(figsize=(10, 6))
 # for i in range(1):
-# 	plt.plot(rotation_phase, power_spectrum[:,i], color=colors[i], marker='o', alpha=alpha, label=r'$k={}$'.format(i+1))
+# 	plt.plot(rotation_phase, power_spectrum[i,:], color=colors[i], marker='o', alpha=alpha, label=r'$k={}$'.format(i+1))
 # plt.xlim([0.2,0.8])
 # plt.xlabel('Stellar rotation phase')
 # plt.ylabel('Amplitude A_k')
